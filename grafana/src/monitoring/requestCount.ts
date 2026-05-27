fa713ff82a7d22b1bc1cc91533860baf6b1a2aa7
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-05-27 20:35:08 +0530

    request count middleware implemented

--- grafana/src/monitoring/requestCount.ts
+++ grafana/src/monitoring/requestCount.ts
@@ -1,10 +1,30 @@
-export function requestCount(req: any, res: any, next: any) {
+import type { NextFunction, Request, Response } from "express";
+import client from "prom-client"
 
-    let count: number = 0;
-    if (req.url == "/user") {
-        count = count + 1
 
-        console.log("Request count is : ", count)
-    }
+const requestCounter = new client.Counter({
+    name: "request_counter",
+    help: "Total request count ",
+    labelNames: ["method", "route", "status"]
+});
+
+
+export function requestCounterMiddleware(req: Request, res: Response, next: NextFunction) {
+    const startTime = Date.now();
+
+    res.on('finish', () => {
+
+        const endTime = Date.now();
+        const duration = endTime - startTime;
+
+        console.log(`Request Took ${duration} ms`);
+
+        requestCounter.inc({
+            method: req.method,
+            route: req.url,
+            status: res.statusCode
+        });
+    });
+
     next()
 }
