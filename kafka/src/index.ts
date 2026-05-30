7032e2e160e8b34269f9673d7f6d825f7b157b18
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-05-30 22:26:29 +0530

    code of kafka producer and the consumer

--- kafka/src/index.ts
+++ kafka/src/index.ts
@@ -1,1 +1,45 @@
 import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+    clientId: "my-app",
+    brokers: ["localhost:9092"],
+})
+
+const producer = kafka.producer();
+const consumer = kafka.consumer({
+    groupId: 'test-group'
+})
+
+
+const run = async () => {
+    await producer.connect();
+    await producer.send({
+        topic: 'test-topic',
+        messages: [
+            { value: 'hello from inside the kafka producer' }
+        ]
+    })
+    await producer.disconnect();
+}
+
+await consumer.connect();
+
+await consumer.subscribe({
+    topic:'test-topic',
+    fromBeginning : false
+})
+
+
+await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+        console.log({
+            partition,
+            topic,
+            value: message.value?.toString(),
+            offset: message.offset
+        })
+    }
+})
+
+
+run().catch(console.error)
